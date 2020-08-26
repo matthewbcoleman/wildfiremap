7420026aa4003c0550d2872b351fa8d7298f0e13
= <matthewbcoleman@ucsb.edu>
2020-08-26 13:32:42 -0700

    daily updates

--- runner.R
+++ runner.R
@@ -4,5 +4,11 @@ library(shiny)
 library(shinyFiles)
 library(rmarkdown)
 
-render('/Users/matthewcoleman/Documents/GitHub/wildfiremap/wildfire_report.Rmd')
+#Download file, unzip, and read in fire data
+temp <- tempfile()
+url <- 'https://firms.modaps.eosdis.nasa.gov/data/active_fire/suomi-npp-viirs-c2/shapes/zips/SUOMI_VIIRS_C2_Global_24h.zip'
+download.file(url,temp)
+unzip(temp, exdir = '/Users/matthewcoleman/Documents/GitHub/wildfiremap/data')
+#unlink(temp)
 
+rmarkdown::render('/Users/matthewcoleman/Documents/GitHub/wildfiremap/wildfire_report.Rmd')
