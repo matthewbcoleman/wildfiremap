3df6552086655f9b98366a287a73317184907bb7
= <matthewbcoleman@ucsb.edu>
2020-08-26 14:44:21 -0700

    daily updates

--- runner.R
+++ runner.R
@@ -7,24 +7,25 @@ library(rmarkdown)
 setwd('/Users/matthewcoleman/Documents/GitHub/wildfiremap')
 
 #Download file, unzip, and read in fire data
-temp <- tempfile()
-print('no error here 1')
+#temp <- tempfile()
 url <- 'https://firms.modaps.eosdis.nasa.gov/data/active_fire/suomi-npp-viirs-c2/shapes/zips/SUOMI_VIIRS_C2_Global_24h.zip'
-print('no error here 2')
-download.file(url,temp)
-print('no error here 3')
-unzip(temp, exdir = '/Users/matthewcoleman/Documents/GitHub/wildfiremap/data')
-print('no error here 4')
-unlink(temp)
-print('no error here 5')
+
+download.file(url,'/Users/matthewcoleman/Documents/GitHub/wildfiremap/data/data.zip')
+
+unzip('/Users/matthewcoleman/Documents/GitHub/wildfiremap/data/data.zip',
+      exdir = '/Users/matthewcoleman/Documents/GitHub/wildfiremap/data')
+
+# #unlink(temp)
+
 
 rmarkdown::render('/Users/matthewcoleman/Documents/GitHub/wildfiremap/wildfire_report.Rmd')
 
-file.remove(c('/Users/matthewcoleman/Documents/GitHub/wildfiremap/data/SUOMI_VIIRS_C2_Global_24h.cpg',
+file.remove(c('/Users/matthewcoleman/Documents/GitHub/wildfiremap/data/data.zip',
+              '/Users/matthewcoleman/Documents/GitHub/wildfiremap/data/SUOMI_VIIRS_C2_Global_24h.cpg',
               '/Users/matthewcoleman/Documents/GitHub/wildfiremap/data/SUOMI_VIIRS_C2_Global_24h.dbf',
               '/Users/matthewcoleman/Documents/GitHub/wildfiremap/data/SUOMI_VIIRS_C2_Global_24h.prj',
               '/Users/matthewcoleman/Documents/GitHub/wildfiremap/data/SUOMI_VIIRS_C2_Global_24h.shp',
               '/Users/matthewcoleman/Documents/GitHub/wildfiremap/data/SUOMI_VIIRS_C2_Global_24h.shx'
               ))
 
-print('no error here end')
+print('Job Well Done!')
